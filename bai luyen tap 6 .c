8cefde86f1c8dd1f5163a968a7209670d88e8fdd
ngminhduc1412007-dotcom <ngminhduc1412007@gmail.com>
2025-10-28 23:32:44 +0700

    Add files via upload

--- bai luyen tap 6 .c
+++ bai luyen tap 6 .c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main(){
-	int m3;
+	int soNuoc;
 	int tongTien;
 	
 	printf ("nhap so nuoc tieu thu: ");
-	scanf ("%d",&m3);
+	scanf ("%d",&soNuoc);
 	
-	if (m3 < 0){
+	if (soNuoc < 0){
 		printf ("so nuoc khong hop le");
 	}
-	else if (m3 <= 10){
-		tongTien = 10 * 6000 + (m3 - 10) * 7000;
+	else if (soNuoc <= 10){
+		tongTien = 10 * 6000 + (soNuoc - 10) * 7000;
 	}
-	else if (m3 <= 30) {
-        tongTien = 10 * 6000 + 10 * 7000 + (m3 - 20) * 8500;
+	else if (soNuoc <= 30) {
+        tongTien = 10 * 6000 + 10 * 7000 + (soNuoc - 20) * 8500;
     } 
     else {
-        tongTien = 10 * 6000 + 10 * 7000 + 10 * 8500 + (m3 - 30) * 10000;
+        tongTien = 10 * 6000 + 10 * 7000 + 10 * 8500 + (soNuoc - 30) * 10000;
     }
      printf("so tien phai tra la: %d \n", tongTien);
 	
